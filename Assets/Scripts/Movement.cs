af42f9cf82bc34aee9262636441178c0e2734570
Stefan Kerekes <posackalaearsack@gmail.com>
2017-09-03 14:52:17 -0400

    First commit by stefan, adding lots of new stuff

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour {
+
+	void Update () {
+
+        CheckLeft();
+        CheckRight();
+	}
+
+    void CheckLeft()
+    {
+        Vector3 pos = gameObject.transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 newpos = new Vector3(Convert.ToSingle(pos.x - 0.1), pos.y, pos.z);
+            gameObject.transform.SetPositionAndRotation(newpos, Quaternion.identity);
+        }
+    }
+
+    void CheckRight()
+    {
+        Vector3 pos = gameObject.transform.position;
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 newpos = new Vector3(Convert.ToSingle(pos.x + 0.1), pos.y, pos.z);
+            gameObject.transform.SetPositionAndRotation(newpos, Quaternion.identity);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
